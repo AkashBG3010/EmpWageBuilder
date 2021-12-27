2c62827941523f963f17cce1780bf7e159812be6
akashbg3010 <akashbpura@gmail.com>
2021-12-27 18:01:40 +0530

    Updated wage calculation per hour

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -4,13 +4,16 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		// Constants
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		int IS_FULL_TIME = 1;
 		// Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is absent");
+			empHrs = 8;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
